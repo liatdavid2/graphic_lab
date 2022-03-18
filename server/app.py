7676770bec5362c4cfd7797b834f9321f3df0812
liatdavid2 <liatdavid2@gmail.com>
2022-03-18 11:33:45 +0200

    flask yolo5 work on image

--- server/app.py
+++ server/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import torch
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    # Model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+    # Images
+    imgs = ['dog.jpg']  # batch of images
+    # Inference
+    results = model(imgs)
+    # Results
+    results.print()
+    results.save()  # or .show()
     return 'TEST'
 
 
